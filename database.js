777f893306529571a9cbc84e707be246e62f9cb9
Andrey Panteleyev <insonifi@gmail.com>
2013-05-25 21:04:44 +0400

    fix mistype in collection acquisition

--- database.js
+++ database.js
@@ -97,7 +97,7 @@ Proto.getModuleList = function(request) {
 		stream.on('data', function (doc) {
 			list.push(doc);
 			//console.log('[database]'.grey, 'got', doc);
-			Proto.collection[doc.id] = client.collection(doc.id);
+			Proto.collections[doc.id] = client.collection(doc.id);
 		});
 		stream.on('end', function() {
 			console.info('[database]'.grey, 'found', list == undefined ? '0' : list.length, 'in track modules list');
