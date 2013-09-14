eee381373493ee52489b1cb9dd73697eec49801a
Andrey <insonifi@gmail.com>
2013-09-14 10:23:23 -0400

    fix parsing coordinates in NMEA parsing module

--- nmea.js
+++ nmea.js
@@ -20,8 +20,10 @@ exports.parse = function (input) {
 	//validate string, return if invalid
 	var validatedStr = validate_nmea(input),
 		nmea = {},
-		data,
-		code;
+		data = [],
+		one_min = 0.0166666666667,
+		knot_to_kph = 1.8519993258722454,
+		code= '';
 	if (!validatedStr) {return 'invalid checksum'; }
 
     //find code and appropriate parser
@@ -37,11 +39,11 @@ exports.parse = function (input) {
 			data[0].slice(4, 6)
 		))).valueOf()//store date as integer//.toISOString();//to store as ISO strings
 		//nmea.isValid = data[1] == 'A';
-		nmea.lat = parseFloat(((data[2] / 100) * 0.0166666666667).toFixed(6));
+		nmea.lat = parseFloat(((data[2].slice(0,2) | 0) + (data[2].slice(2) * one_min)).toFixed(6));
 		if (data[3] === 'S') {nmea.lat = -nmea.lat; }
-		nmea.long = parseFloat(((data[4] / 100) * 0.0166666666667).toFixed(6));
+		nmea.long = parseFloat(((data[2].slice(0,2) | 0) + (data[2].slice(2) * one_min)).toFixed(6));
 		if (data[5] === 'W') {nmea.long = -nmea.long; }
-		nmea.kph = (data[6] * 1.8519993258722454).toFixed(2);
+		nmea.kph = (data[6] * knot_to_kph).toFixed(2);
 		nmea.track = data[7];
 		nmea.magv = data[9];
 		nmea.isValid = true;
